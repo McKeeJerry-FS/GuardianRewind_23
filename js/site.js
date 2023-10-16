7bd6c474df2e291b6158f6d1fd7bd49fde9ed298
Jerry McKee Jr <JerryMcKeeJr.Dev@gmail.com>
2023-10-16 15:07:32 -0400

    created HTML and getValues function

--- js/site.js
+++ js/site.js
@@ -1,12 +1,32 @@
-function displayMessage() {
-    // get the text out of input and assign to a variable
-    let msg = document.getElementById('message').value;
-    
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg, // <-- replace this with the variable
-        }
-    );
+// get the user's message to reverse
+function getValues(){
+    // get the text input from the page
+    let msg = document.getElementById('userInput').value;
+
+    // Validate the input : make sure it's not empty
+    //                      make sure it is at least two characters (optional)
+    if(msg.length == 0){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops',
+            message: 'Please enter a message to reverse',
+        });
+    }
+    else {
+        // pass the input to reverseMessage function and assign its return value to a variable
+        let revMsg = reverseMessage(msg);
+
+        // give the reversed message to displayReversedMessage
+        displayReversedMessage(revMsg);
+    }
+}
+
+// reverse the string 
+function reverseMessage(message){
+    let msg = message;
+}
+
+// displays the reversed message
+function displayReversedMessage(){
+
 }
